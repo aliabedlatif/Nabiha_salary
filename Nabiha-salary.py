fb80318c91f66cc96a82d83af46376f15de50602
DESKTOP-1GLGR54 <ali@gmail.com>
2025-02-21 01:19:32 +0200

    Add if statement for the three option

--- Nabiha-salary.py
+++ Nabiha-salary.py
@@ -2,3 +2,10 @@ salary = int(input("Enter salary of month:"))
 Month = input("Enter name of month:")
 percentage = input("Enter price for a)saving , b)rent ,c)electricty:")
 while percentage != "d":
+    if percentage == "a":
+        save = int(input("the amount saving:"))
+    elif percentage == "b":
+        rent = int(input("the amount rent:"))
+    elif percentage == "c":
+        electricty = int(input("the amount electricty:"))
+    percentage = input("enter price a)saving b)rent c)electricty: ")
