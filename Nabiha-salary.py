1228e9cbd8edaf56638416163c6ec9095ad9aa65
DESKTOP-1GLGR54 <ali@gmail.com>
2025-02-21 01:13:23 +0200

    Add while loop

--- Nabiha-salary.py
+++ Nabiha-salary.py
@@ -1,3 +1,4 @@
 salary = int(input("Enter salary of month:"))
 Month = input("Enter name of month:")
 percentage = input("Enter price for a)saving , b)rent ,c)electricty:")
+while percentage != "d":
